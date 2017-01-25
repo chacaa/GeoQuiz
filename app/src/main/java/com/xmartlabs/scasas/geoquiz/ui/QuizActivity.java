20c384948b30b7aaa6f9f29afe26a3f924b81abf
chacaa <scasas@xmartlabs.com>
2017-01-25 14:34:16 -0300

    First commit of my first andorid app (#1)

---
+++ app/src/main/java/com/xmartlabs/scasas/geoquiz/ui/QuizActivity.java
@@ -0,0 +1,60 @@
+package com.xmartlabs.scasas.geoquiz.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.xmartlabs.scasas.geoquiz.R;
+
+/**
+ * Created by scasas on 1/17/17
+ */
+
+public class QuizActivity extends AppCompatActivity {
+  private Button falseButton;
+  private Button trueButon;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_quiz);
+    setupQuizButton();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.quiz, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      // TODO: 1/24/17
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void setupQuizButton() {
+    trueButon = (Button) findViewById(R.id.true_button);
+    trueButon.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+      }
+    });
+    falseButton = (Button) findViewById(R.id.false_button);
+    falseButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+}
