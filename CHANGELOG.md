1af7c60c26b0614e515d418284fe687e503be1a4
chacaa <scasas@xmartlabs.com>
2017-02-02 15:00:16 -0300

    Fabric integration and preparation for realease v1.0.0 (#4)

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# CHANGELOG
+All notable changes will be documented in this file.
+
+### [1.0.0](https://github.com/chacaa/GeoQuiz/releases/tag/v1.0.0)
+<!-- Released on 2017-02-02. -->
+* Adds a cheat button to the app
+* Add more questions to the application and also some improvements from the last version
+* First commit of my first andorid app
+
+[xmartlabs]: https://xmartlabs.com
--- app/src/main/java/com/xmartlabs/scasas/geoquiz/ui/QuizActivity.java
+++ app/src/main/java/com/xmartlabs/scasas/geoquiz/ui/QuizActivity.java
@@ -14,12 +14,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.xmartlabs.scasas.geoquiz.CheatActivity;
 import com.xmartlabs.scasas.geoquiz.R;
 import com.xmartlabs.scasas.geoquiz.module.Question;
 
 import java.util.ArrayList;
 
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
@@ -46,6 +48,7 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    initializeFabric();
     setContentView(R.layout.activity_quiz);
     bindViews();
     if (savedInstanceState != null) {
@@ -271,5 +274,9 @@ public class QuizActivity extends AppCompatActivity {
     prevButton = (ImageButton) findViewById(R.id.prev_button);
     questionTextView = (TextView) findViewById(R.id.question);
     trueButton = (Button) findViewById(R.id.true_button);
+  }
+
+  private void initializeFabric() {
+    Fabric.with(this, new Crashlytics());
   }
 }
